ca7d74af855c19e94d05d1ee62167a68a4bceabd
Kenneth D. Evensen <kevensen@redhat.com>
2017-01-20 09:59:01 -0500

    Cleaner error handling.

--- POC/Service/Provisioning/StateMachines/ServiceProvision_Template.class/__methods__/update_serviceprovision_status_delete_project.rb
+++ POC/Service/Provisioning/StateMachines/ServiceProvision_Template.class/__methods__/update_serviceprovision_status_delete_project.rb
@@ -4,17 +4,18 @@
 #
 
 prov = $evm.root['service_template_provision_task']
+service = prov.destination
 
 # Get status from input field status
 status = $evm.inputs['status']
 
 $evm.instantiate("/POC/Containers/Methods/DeleteProject")
-
+service.remove_from_vmdb
 # Update Status for on_entry,on_exit
 if $evm.root['ae_result'] == 'ok' || $evm.root['ae_result'] == 'error'
   prov.message = status
 end
 
-$evm.root['ae_result'] = 'finished'
-$evm.root["miq_request"].deny("admin", "Failed to complete")
-
+$evm.root['ae_result'] = 'error'
+prov.miq_request.deny("admin", prov.message)
+exit MIQ_ERROR
